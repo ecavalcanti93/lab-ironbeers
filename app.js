50791fedd4952541ecc9e6289590494ff3ebf301
ecavalcanti683 <ecavalcanti.dg@outlook.com>
2023-11-27 18:27:24 +0100

    Almost Done - CSS

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(__dirname + '/views/partials');
 // ...
 
 // Add the route handlers here:
@@ -21,5 +21,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => res.render('beers', { beers }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  const randomBeer = punkAPI
+    .getRandom()
+    .then(randomBeer => {res.render('random-beer', { randomBeer: randomBeer})})
+    .catch(error => {console.log(error)}
+)});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
